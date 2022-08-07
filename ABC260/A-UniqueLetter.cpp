386547712bd8e096aee7750885c07db22b2da974
UmezakiDaisuke <60737352+UmezakiDaisuke@users.noreply.github.com>
2022-08-08 08:53:04 +0900

    解説AC

---
+++ ABC260/A-UniqueLetter.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdtr1c++.h>
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define repi(i, a, b) for (int i = (int)(a); i < (int)(b); i++)
+using namespace std;
+
+int main()
+{
+  string s;
+  cin >> s;
+  if (s[0] != s[1] && s[0] != s[2])
+  {
+    cout << s[0] << endl;
+  }
+  else if (s[1] != s[0] && s[1] != s[2])
+  {
+    cout << s[1] << endl;
+  }
+  else if (s[2] != s[0] && s[2] != s[1])
+  {
+    cout << s[2] << endl;
+  }
+  else
+  {
+    cout << "-1" << endl;
+  }
+}
